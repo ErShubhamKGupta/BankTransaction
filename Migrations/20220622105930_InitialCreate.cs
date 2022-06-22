05e9fa176e7a0fdaf783925d23b72ae8b2dfbe69
ErShubhamKGupta <shubham.7368@gmail.com>
2022-06-22 22:23:28 +0530

    Add project files.

---
+++ Migrations/20220622105930_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankTransaction.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountNumber = table.Column<string>(type: "nvarchar(12)", nullable: false),
+                    BeneficiaryName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    BankName = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    IFSCCode = table.Column<string>(type: "nvarchar(10)", nullable: false),
+                    Ammount = table.Column<int>(type: "int", nullable: false),
+                    TransactionDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTransaction.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        [Required]
+        [MaxLength(12,ErrorMessage ="Account Number must be 12 digits only!")]
+        [MinLength(12, ErrorMessage = "Account Number must be 12 digits only!")]
+        [DisplayName("Account Number")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        [DisplayName("Beneficiary Name")]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        [DisplayName("Bank Name")]
+        public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        [Required]
+        [MaxLength(8, ErrorMessage = "IFSC Code must be 8 characters only!")]
+        [MinLength(8,ErrorMessage = "IFSC Code must be 8 characters only!")]
+        [DisplayName("IFSC Code")]
+        public string IFSCCode { get; set; }
+
+        [Required]
+        [DisplayName("Amount")]
+        public int Ammount { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:MMM-dd-yy}")]
+        [DisplayName("Date")]
+        public DateTime TransactionDate { get; set; }
+    }
+}
